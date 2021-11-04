86a1393b8153ae6c87be79b6e6147201458542e9
Sincan <sincanmaulanaa@gmail.com>
2021-11-04 12:56:15 +0700

    initializing project

--- bin/simple_grade_classification.dart
+++ bin/simple_grade_classification.dart
@@ -3,18 +3,19 @@ import 'dart:io';
 void main() {
   stdout.write("Masukkan nilai: ");
   var user = int.parse(stdin.readLineSync()!);
-
+  var grade;
   if(user >= 90 && user <= 100) {
-    print("Anda mendapatkan grade A");
+    grade = 'A';
   } else if(user >= 80 && user <= 89) {
-    print("Anda mendapatkan grade B");
+    grade = 'B';
   } else if(user >= 70 && user <= 79) {
-    print("Anda mendapatkan grade C");
+    grade = 'C';
   } else if(user >= 50 && user <= 69) {
-    print("Anda mendapatkan grade D");
+    grade = 'D';
   } else if(user >= 0 && user <= 49) {
-    print("Anda mendapatkan grade E");
+    grade = 'E';
   } else {
     print("Nilai yang Anda inputkan harus diantara 0 - 100");
   }
+  print("Anda mendapatkan grade $grade");
 }
